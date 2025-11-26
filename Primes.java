7e82f58733f305dd56fd8bf6cbd4004b4fb1fa1e
Tal Gershtein <tal.gershtein@post.runi.ac.il>
2025-11-26 16:03:32 +0200

    Completed assignment

--- Primes.java
+++ Primes.java
@@ -1,5 +1,26 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int num = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[num + 1];
+        arr[0] = false; arr[1] = false;
+        for(int i = 2; i<arr.length; i++)
+            arr[i] = true;
+        int count = 0;
+        System.out.println("Prime numbers up to " + num + ":");
+        for(int i = 2; i < arr.length; i++) {
+            if(arr[i]) {
+                change(arr,i);
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("There are " + count + " primes between 2 and " + num + " (" + count*100/num + "% are primes)");
+    }
+    public static void change(boolean[] arr, int prime){
+        int count = prime * 2;
+        while (count < arr.length) {
+            arr[count] = false;
+            count += prime;
+        }
     }
 }
